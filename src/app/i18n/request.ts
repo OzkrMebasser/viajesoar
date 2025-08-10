b8d7479c860be8e80e788bcf3d5e22d2efe2eeb2
OzkrMebasser <oscarmorenoestudios@gmail.com>
2025-08-10 00:04:27 -0700

    Fixing locales to preview in vercel

--- src/app/i18n/request.ts
+++ src/app/i18n/request.ts
@@ -1,14 +1,16 @@
 import { getRequestConfig } from 'next-intl/server';
 import { routing } from '@/app/i18n/routing';
+import { getMessages } from 'next-intl/server';
 
-
-export default getRequestConfig(async ({ locale }) => {
-  if (!routing.locales.includes(locale as any)) {
-    locale = routing.defaultLocale; // Fallback
+export default getRequestConfig(async ({ locale }: { locale?: string }) => {
+  if (!locale || !routing.locales.includes(locale as any)) {
+    locale = routing.defaultLocale; // fallback seguro
   }
 
+  const messages = await getMessages();
+
   return {
-    locale, // 👈 IMPORTANTE: incluirlo siempre
-    messages: (await import(`../messages/${locale}.json`)).default
+    locale,
+    messages,
   };
 });
